468a429c696da123c26174283c0aaececdf3c29e
Tim Millar <heteroskedasticity99@gmail.com>
2018-11-24 23:19:27 +0000

    basic appointments form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,19 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointment = Appointment.new
+    @appointments = Appointment.order(time: :asc)
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'appointments/index', controller: 'appointments', action: 'index'
+
+  resources :appointments, only: [:index, :create]
 end
