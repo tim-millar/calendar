ba6a6bb393d6b992d3106c04c1d5e33fab1d6a1c
Tim Millar <heteroskedasticity99@gmail.com>
2018-11-24 15:05:55 +0000

    initial setup of app with one react component

---
+++ app/javascript/components/Appointments.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Appointments extends React.Component {
+	render () {
+		return (
+			<h1>
+				This is an appointment!
+			</h1>
+		)
+	}
+}
+
+export default Appointments
---
+++ app/javascript/components/Foo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Foo extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Foo Bar
+      </React.Fragment>
+    );
+  }
+}
+
+export default Foo
