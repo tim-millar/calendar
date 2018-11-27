40caf2e96928b5bab7a18dba7e3778f140bb06cf
Tim Millar <heteroskedasticity99@gmail.com>
2018-11-27 11:43:00 +0000

    allow users to update UI

--- app/javascript/components/AppointmentForm.jsx
+++ app/javascript/components/AppointmentForm.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
 class AppointmentForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event) {
+		var name = event.target.name
+		var obj = {}
+		obj[name] = event.target.value
+		this.props.onUserInput(obj)
+	}
+
 	render () {
 		return (
 			<form className="form card">
-				<div class="fields">
+				<div className="fields">
 					<div className="input-text">
 						<label htmlFor="title">Appointment Title</label><br/>
-						<input type="text" name="title" placeholder="Appointment Title" /><br/>
+						<input
+							type="text"
+							name="title"
+							value={this.props.title}
+							onChange={this.handleChange} />
+						<br/>
 						<label htmlFor="time">Time</label><br/>
-						<input type="text" name="time" placeholder="Date and Time" /><br/>
+						<input
+							type="date"
+							name="time"
+							value={this.props.time}
+							onChange={this.handleChange} />
+						<br/>
 					</div>
 					<div className="submit-button">
 						<input type="submit" value="Make Appointment" />
--- app/javascript/components/Appointments.jsx
+++ app/javascript/components/Appointments.jsx
@@ -5,11 +5,28 @@ import AppointmentForm from './AppointmentForm';
 import AppointmentsList from './AppointmentsList';
 
 class Appointments extends React.Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			appointments: this.props.appointments,
+			title: 'This is your first appointment',
+			time: '2019-01-02',
+		}
+		this.handleUserInput = this.handleUserInput.bind(this)
+	}
+
+	handleUserInput (obj) {
+		this.setState(obj)
+	}
+
 	render () {
 		return (
 			<div>
-				<AppointmentForm />
-				<AppointmentsList appointments={this.props.appointments} />
+				<AppointmentForm
+					title={this.state.title}
+					time={this.state.time}
+					onUserInput={this.handleUserInput} />
+				<AppointmentsList appointments={this.state.appointments} />
 			</div>
 		)
 	}
