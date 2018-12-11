eaee50c7eefde55dc41fab7e852e2470772170a9
Tim Millar <heteroskedasticity99@gmail.com>
2018-12-11 00:19:20 +0000

    config of enzyme and jest for testing & first test

---
+++ test/javascript/components/appointment.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme'
+import  Appointment from '../../../app/javascript/components/Appointment';
+
+describe('Appointment', () => {
+	it('should render the title and date', () => {
+		const wrapper = mount(
+			<Appointment title={'Appointment'} time={'2018-03-30'} />
+		)
+		expect(wrapper.html()).toEqual(
+			'<div class=\"card\"><h3>Appointment</h3><p>March 30 2018, 12:00:00 am</p></div>'
+		)
+		wrapper.unmount();
+	})
+})
