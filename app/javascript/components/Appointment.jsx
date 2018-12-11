4c83f63cd2f3f8449a2d8dfb5800cecbb615cc6f
Tim Millar <heteroskedasticity99@gmail.com>
2018-12-11 01:04:48 +0000

    add proptypes to appointment component

--- app/javascript/components/Appointment.jsx
+++ app/javascript/components/Appointment.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { formatDate } from './utils';
 
 class Appointment extends React.PureComponent {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired
+  }
+
 	render () {
 		return (
 			<div className="card">
