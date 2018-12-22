800558087503bc0617c82fd456f97a432f618e0a
Tim Millar <heteroskedasticity99@gmail.com>
2018-12-22 00:02:25 +0000

    WIP submit form from button, clear input

--- app/javascript/components/AppointmentForm.jsx
+++ app/javascript/components/AppointmentForm.jsx
@@ -1,83 +1,89 @@
-import React from 'react';
+import React from 'react'
 import DateTime from 'react-datetime'
-import Input from '@material-ui/core/Input';
-import Icon from '@material-ui/core/Icon';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input'
+import Icon from '@material-ui/core/Icon'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
 import CalendarToday from '@material-ui/icons/CalendarToday'
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
 import Label from './Label'
 import '../../../node_modules/react-datetime/css/react-datetime.css'
-import { contextWrapper } from './context';
+import { contextWrapper } from './context'
 
 class AppointmentForm extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props)
+  }
 
-	handleChange = (event) => {
-		const name = event.target.name
-		let obj = {}
-		obj[name] = event.target.value
-		this.props.onUserInput(obj)
-	}
+  handleChange = event => {
+    const name = event.target.name
+    let obj = {}
+    obj[name] = event.target.value
+    this.props.onUserInput(obj)
+  }
 
-	handleSubmit = (event) => {
-		event.preventDefault()
-		this.props.onFormSubmit()
-	}
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.onFormSubmit()
+  }
 
-	setTime = (event) => {
-		const name = 'time'
-		let obj = {}
-		if (obj[name] = event.toDate()) {
-			this.props.onUserInput(obj)
-		}
-	}
+  setTime = event => {
+    const name = 'time'
+    let obj = {}
+    if ((obj[name] = event.toDate())) {
+      this.props.onUserInput(obj)
+    }
+  }
 
-	render () {
-		let inputProps = {
-			name: 'time'
-		}
-		return (
-			<form id="form" className="form card" onSubmit={this.handleSubmit}>
-				<div className="fields">
-					<div className="input-text">
-						<TextField
-							label="Make an appointment"
-							type="text"
-							name="title"
-							fullWidth={true}
-							margin="normal"
-							variant="outlined"
-							onChange={this.handleChange}
-							InputProps={{
-								endAdornment: (
-									<InputAdornment postition="start">
-										<CalendarToday />
-									</InputAdornment>
-							)}}
-						/>
-						<br/>
-						<DateTime
-							input={false}
-							open={true}
-							inputProps={inputProps}
-							value={this.props.time}
-							onChange={this.setTime} />
-				    <Button
-			        variant="contained"
-			        color="primary"
-			        align="center"
-			        fullWidth={true} >
-				      Make Appointment
-			      </Button>
-						<br/>
-					</div>
-				</div>
-		  </form>
-		)
-	}
+  render() {
+    let inputProps = {
+      name: 'time',
+    }
+    return (
+      <form id="form" className="form card" onSubmit={this.handleSubmit}>
+        <div className="fields">
+          <div className="input-text">
+            <TextField
+              label="Make an appointment"
+              type="text"
+              name="title"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              value={this.props.title}
+              onChange={this.handleChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment postition="start">
+                    <CalendarToday />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <br />
+            <DateTime
+              input={false}
+              open={true}
+              inputProps={inputProps}
+              value={this.props.time}
+              onChange={this.setTime}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              align="center"
+              type="submit"
+              form="form"
+              fullWidth={true}
+            >
+              Make Appointment
+            </Button>
+            <br />
+          </div>
+        </div>
+      </form>
+    )
+  }
 }
 
-export default contextWrapper(AppointmentForm);
+export default contextWrapper(AppointmentForm)
--- app/javascript/components/Appointments.jsx
+++ app/javascript/components/Appointments.jsx
@@ -1,16 +1,16 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react'
+import PropTypes from 'prop-types'
 import axios from 'axios'
 import update from 'immutability-helper'
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader'
 
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography'
+import AppBar from '@material-ui/core/AppBar'
 
-import AppointmentForm from './AppointmentForm';
-import AppointmentsList from './AppointmentsList';
-import Appointment from './Appointment';
-import { contextWrapper, Provider } from './context';
+import AppointmentForm from './AppointmentForm'
+import AppointmentsList from './AppointmentsList'
+import Appointment from './Appointment'
+import { contextWrapper, Provider } from './context'
 
 import 'typeface-roboto'
 
@@ -20,84 +20,81 @@ import 'typeface-roboto'
 // }
 
 class Appointments extends React.Component {
-	constructor (props) {
-		super(props)
-		this.state = {
-			appointments: this.props.appointments,
-			title: 'Make an appointment',
-			time: 'DD-MM-YYYY',
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      appointments: this.props.appointments,
+      title: 'Make an appointment',
+      time: 'DD-MM-YYYY',
+    }
+  }
 
-	handleUserInput = (obj) => {
-		this.setState(obj)
-	}
+  handleUserInput = obj => {
+    this.setState(obj)
+  }
 
-	handleFormSubmit = () => {
-		const appointment = {
-			title: this.state.title,
-			time: this.state.time,
-		}
+  handleFormSubmit = () => {
+    const appointment = {
+      title: this.state.title,
+      time: this.state.time,
+    }
 
-		axios({
-			method: 'post',
-			url: '/appointments',
-			data: { appointment },
-			headers: {
-				'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
-			}
-		}).then((response) => {
-			this.addNewAppointment(response.data)
-		})
-	}
+    axios({
+      method: 'post',
+      url: '/appointments',
+      data: { appointment },
+      headers: {
+        'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').content,
+      },
+    }).then(response => {
+      this.addNewAppointment(response.data)
+    })
+  }
 
-	addNewAppointment = (appointment) => {
-		const appointments = update(
-			this.state.appointments, { $push: [appointment] }
-		)
-		this.setState({
-			title: 'Make an appointment',
-			time: 'DD-MM-YYYY',
-			appointments: appointments.sort(function(a,b) {
-				return new Date(a.time) - new Date(b.time)
-		})})
-	}
+  addNewAppointment = appointment => {
+    const appointments = update(this.state.appointments, {
+      $push: [appointment],
+    })
+    this.setState({
+      title: '',
+      time: 'DD-MM-YYYY',
+      appointments: appointments.sort(function(a, b) {
+        return new Date(a.time) - new Date(b.time)
+      }),
+    })
+  }
 
-	render () {
-		const context = {
-			onUserInput: this.handleUserInput,
-			onFormSubmit: this.handleFormSubmit
-		}
-		return (
-			<div>
-				<AppBar position="sticky">
-					<Typography variant="h1"
-											align="center"
-											color="inherit">
-						Calendar
-					</Typography>
-				</AppBar>
-				<Provider value={context}>
-					<AppointmentForm title={this.state.title} time={this.state.time} />
-				</Provider>
-				<AppointmentsList
-					appointments={this.state.appointments}>
-					{appointments => {
-						return (
-							appointments.map(appointment => {
-								return (
-									<Appointment
-										key={appointment.id}
-										title={appointment.title}
-										time={appointment.time} />
-								)
-							})
-						)
-					}}
-			  </AppointmentsList>
-		  </div>
-		)
-	}
+  render() {
+    const context = {
+      onUserInput: this.handleUserInput,
+      onFormSubmit: this.handleFormSubmit,
+    }
+    return (
+      <div>
+        <AppBar position="sticky">
+          <Typography variant="h1" align="center" color="inherit">
+            Calendar
+          </Typography>
+        </AppBar>
+        <Provider value={context}>
+          <AppointmentForm title={this.state.title} time={this.state.time} />
+        </Provider>
+        <AppointmentsList appointments={this.state.appointments}>
+          {appointments => {
+            return appointments.map(appointment => {
+              return (
+                <Appointment
+                  key={appointment.id}
+                  title={appointment.title}
+                  time={appointment.time}
+                />
+              )
+            })
+          }}
+        </AppointmentsList>
+      </div>
+    )
+  }
 }
 
 export default hot(module)(Appointments)
